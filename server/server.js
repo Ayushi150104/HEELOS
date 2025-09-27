eba2e0e3e266aa8d5adce8417d5a59abea502a24
Ayushi150104 <choudharybinaykumar0@gmail.com>
2025-09-27 13:19:53 +0530

    Final Product

--- server/server.js
+++ server/server.js
@@ -12,7 +12,8 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 const allowedOrigins = [
-  "https://heelos-mb5j6b0x6-ayushi-choudharys-projects-b1319b45.vercel.app",       // deployed frontend
+  "https://heelos-mb5j6b0x6-ayushi-choudharys-projects-b1319b45.vercel.app", 
+  "https://heelos.vercel.app/",      // deployed frontend
   "http://localhost:3000",      // local frontend
   "http://localhost:3001",      // optional
 ];
